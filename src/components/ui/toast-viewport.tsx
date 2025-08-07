dc4ee5d240dbdfc80c5cd67df97d12162cec26b3
LankfordCapital <james@lankfordcapital.com>
2025-08-07 17:16:04 -0400

    Create toast-viewport.tsx

---
+++ src/components/ui/toast-viewport.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ToastViewport = ({ children }: { children: React.ReactNode }) => (
+  <div className="mt-2">{children}</div>
+);
+
+export default ToastViewport;
