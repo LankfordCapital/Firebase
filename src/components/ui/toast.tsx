c2504f50934a73c54df65ae873e371f3c2c8feea
LankfordCapital <james@lankfordcapital.com>
2025-08-07 15:53:14 -0400

    Update toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -47,5 +47,5 @@ const Toast: React.FC<ToastProps> = ({ message, type = "info" }) => {
   );
 };
 
-export { ToastProvider, Toast, ToastTitle, ToastDescription, ToastClose, ToastViewport } from "wherever-they-are-defined";
-
+export default Toast;
+export { Toast };
