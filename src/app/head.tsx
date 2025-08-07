53cf4ec59581c45266598b44dd33a9af788ade60
LankfordCapital <james@lankfordcapital.com>
2025-08-07 16:00:27 -0400

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="icon"
+        href="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
+      />
+    </>
+  );
+}
