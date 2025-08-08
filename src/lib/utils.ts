9a449a6240d7bed91feb5b1155daf5ef56dc4420
LankfordCapital <james@lankfordcapital.com>
2025-08-08 13:01:43 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
-export function cn(...classes: Array<string | false | null | undefined>) {
+export function cn(
+  ...classes: Array<string | false | null | undefined>
+): string {
   return classes.filter(Boolean).join(' ');
 }
